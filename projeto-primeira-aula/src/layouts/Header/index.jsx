945092078ef47e78e45abd66d53b363b9fbc5621
FELIPE CARDOSO DA COSTA <felipe_cardoso_costa@estudante.sc.senai.br>
2026-02-10 22:13:40 -0300

    modulo de css

--- projeto-primeira-aula/src/layouts/Header/index.jsx
+++ projeto-primeira-aula/src/layouts/Header/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 
+import styles  from'./Header.module.css'
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
         <h1>Titulo</h1>
     </header>
   )
