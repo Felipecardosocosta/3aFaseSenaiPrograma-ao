970466658a65125c30fc4e290e0609c30a7c0ef6
FELIPE CARDOSO DA COSTA <felipe_cardoso_costa@estudante.sc.senai.br>
2026-02-23 22:19:45 -0300

    Aula 01 - Introdução ao jest no backend

--- aula-teste/SA/SA/api/tests/sum.test.js
+++ aula-teste/SA/SA/api/tests/sum.test.js
@@ -16,3 +16,58 @@ describe('the can', () => {
     expect(can.name).toBe('pamplemousse');
   });
 }); 
+
+
+const testeTestado = {
+    testeNome: 'seuNome',
+    suaIdade: 30
+}
+
+describe("The TesteTestado", ()=>{
+
+    test('teste seu nome', ()=>expect(testeTestado.testeNome).toBe("seuNome") );
+
+    test("teste sua idade", ()=>expect(testeTestado.suaIdade).toBe(30));
+
+})
+
+
+const pessoaUm = {
+    nome:"pessoa1",
+    idade: 10.5
+}
+const pessoaDois = {
+    nome:"pessoa1",
+    idade: 10.5
+}
+
+
+describe("The TesteTestadoPessoa", ()=>{
+
+    test('Pessoa igual', ()=>expect(pessoaUm).toEqual(pessoaDois) );
+
+ 
+
+})
+
+
+const velho1 = {
+    nome:"pessoa1",
+    idade: 90
+}
+const velho2 = {
+    nome:"pessoa1",
+    idade: 85
+}
+
+
+describe("The TesteTestadoVelho", ()=>{
+
+   test('Velho nao igual', ()=>expect(velho1).not.toEqual(velho2));
+
+})
+
+
+
+
+
